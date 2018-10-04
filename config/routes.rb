beb00e98d17f5ce029374eec9e54d8d5b30e32c3
radlero <radoslaw.lewandowski@arnoldclark.com>
2018-10-04 13:05:20 +0100

    Revert "added redis and dashing gem"
    This reverts commit f9a801f31bf701f8fdc8d3554a8e4472a7600afb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  mount Dashing::Engine, at: Dashing.config.engine_path
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
