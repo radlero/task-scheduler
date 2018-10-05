d201da208259aeff366c24fc124db831137610e3
radlero <radoslaw.lewandowski@arnoldclark.com>
2018-10-05 13:58:02 +0100

    changes to css and styling

--- task_scheduler/jobs/sample_job.rb
+++ task_scheduler/jobs/sample_job.rb
@@ -1,13 +1,13 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '3s', :first_in => 0 do |job|
-  send_event('station1', { text1: "Car Service", time: "14:00"})
+SCHEDULER.every '1s', :first_in => 0 do |job|
+  send_event('station1', { text1: "Car Service", time: "9:00"})
 end
-SCHEDULER.every '10s', :first_in => 0 do |job|
-  send_event('station2', { text1: "Tyre Change", text2: "" })
+SCHEDULER.every '1s', :first_in => 0 do |job|
+  send_event('station2', { text1: "Tyre Change", text2: "" , time: "9:00"})
 end
-  SCHEDULER.every '7s', :first_in => 0 do |job|
-    send_event('station1', { text2: "HAAHA it is working"})
+  SCHEDULER.every '1s', :first_in => 0 do |job|
+    send_event('station1', { text2: "scheduling is working", time2: "10:00"})
 end
 SCHEDULER.every '20s', :first_in => 0 do |job|
-  send_event('station2', { text1: "Tea Break", text2: "Tyre Change"})
+  send_event('station2', { text1: "Tea Break", text2: "Tyre Change", time: "10:00"})
 end
