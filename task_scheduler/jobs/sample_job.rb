79c9a642435b9dd4aaad3b9c4e20bd64c9d998a8
radlero <radoslaw.lewandowski@arnoldclark.com>
2018-10-05 09:25:28 +0100

    added css for button class and multiple text for widget component

--- task_scheduler/jobs/sample_job.rb
+++ task_scheduler/jobs/sample_job.rb
@@ -1,10 +1,10 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 SCHEDULER.every '3s', :first_in => 0 do |job|
-  send_event('station1', { text: "1 ja pierdole" })
+  send_event('station1', { text: "Car Service", text2: "Tea Break", text3: "" })
 end
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('station2', { text: "2 uda sie" })
+  send_event('station2', { text: "Tyre Change" })
 end
   SCHEDULER.every '7s', :first_in => 0 do |job|
-    send_event('station1', { text: "3 proba" })
+    send_event('station1', { text: "HAAHA it is working", text2: "Tea Break", text3: "New Task"})
 end
