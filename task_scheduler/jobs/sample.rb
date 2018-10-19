ac137fb4b6e1e86316ca9e8753f724db482d631d
radlero <radoslaw.lewandowski@arnoldclark.com>
2018-10-19 09:32:01 +0100

    created job to connect with API

--- task_scheduler/jobs/sample.rb
+++ task_scheduler/jobs/sample.rb
@@ -1,13 +1,26 @@
-current_valuation = 0
-current_karma = 0
-
-SCHEDULER.every '2s' do
-  last_valuation = current_valuation
-  last_karma     = current_karma
-  current_valuation = rand(100)
-  current_karma     = rand(200000)
+require 'net/http'
+require 'json'
+require 'open-uri'
 
 
-  send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: rand(100) })
+def get_data
+  uri = URI("https://taskboard-rl.herokuapp.com/tasks.json")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  response = http.request(Net::HTTP::Get.new(uri.request_uri))
+  JSON.parse(response.body)
+end
+
+SCHEDULER.every "30s", :first_in => 0 do |job|
+
+stations = get_data
+
+# Send Data
+  stations['tasks'].each do |task|
+    data = {
+      text2: task['task'],
+      time2: task['time']
+    }
+    send_event(task['station'], data)
+  end
 end
